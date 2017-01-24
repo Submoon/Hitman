00f9617af743acf7f04ff437eb8be9a6c6fd2e6a
Fabien Bal-Fontaine <fabien_bf@hotmail.fr>
2017-01-24 23:47:59 +0100

    It is alive

--- ping.rb
+++ ping.rb
@@ -24,6 +24,10 @@ bot.message(content: 'Ping!') do |event|
   event.respond 'Pong!'
 end
 
+bot.message(content: 'tts Ping!!') do |event|
+  event.respond '/tts Je vais manger vos parents et étrangler vos yaourts!'
+end
+
 # This method call has to be put at the end of your script, it is what makes the bot actually connect to Discord. If you
 # leave it out (try it!) the script will simply stop and the bot will not appear online.
 bot.run
