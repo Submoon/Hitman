21e8fc69cff8ce837947a361e06ea9b7bb0ea24e
Fabien Bal-Fontaine <fabien_bf@hotmail.fr>
2017-01-26 18:50:16 +0100

    Interpreter for brainfuck working !

--- lib/Hitman/brainfuck_commands.rb
+++ lib/Hitman/brainfuck_commands.rb
@@ -1,24 +1,19 @@
-require 'brainfucktt'
+require 'brainfuck'
 require 'stringio'
 
 module BrainfuckCommands
   extend Discordrb::Commands::CommandContainer
 
-  command(:bf, description: 'Interpretes brainfuck') do |event, *args|
+  command(:bf, description: 'Interprater for brainfuck') do |event, *args|
     begin
       code = args.last
       output = StringIO.new
-      parser = Brainfucktt.parse(code)
-      Brainfucktt.run(code, output: output)
+      parser = Brainfuck.new(code, output)
+      parser.run()
       output.string
-    rescue RangeError => e
-      event.channel.send_message "Impossible to parse due to\n\t #{e.message}"
-    else
-      event.channel.send_message 'Unknown Error'
-    ensure
-      event.channel.send_message "Pointer value: #{Brainfucktt::Parser.instance.pointer}"
+    rescue Exception => e
+      event.channel.send_message e.message
     end
-
   end
 
 
